ae73da50c7ce05547cca0cacc359b73bf62fd388
Mumbi1 <mutheemumbi01@gmail.com>
2022-10-20 07:55:29 -0700

    prints a single character

---
+++ print_char.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_char - print a single char
+ * @ap: va_list pointer containg a char
+ * @modif: struct modifier with modifier fields
+ * Return: count of one char
+ */
+char *print_char (modifier_t *modif, va_list ap)
+{
+	char *ch;
+	char c;
+
+	if (!ap || !modif)
+		return (0);
+	c = va_arg(ap, int);
+	ch = malloc(2);
+	if (!ch)
+	{
+		return (NULL);
+	}
+	if (c)
+	{
+		ch[0] = c;
+	}
+	else
+	{
+		ch[0] = '\0';
+	}
+	ch[1] = '\0';
+	return (ch);
+}
